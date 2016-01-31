1115e70b80ef22d3533e14c93131d55b9186d6d8
takdeerkaur <takdeerkaur@gmail.com>
2016-01-30 17:09:37 -0800

    Implement add topic functionality

--- app/src/main/java/com/takdeer/conversationstarter/MainActivity.java
+++ app/src/main/java/com/takdeer/conversationstarter/MainActivity.java
@@ -1,6 +1,8 @@
 package com.takdeer.conversationstarter;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Entity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +10,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -38,15 +41,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button topicsButton;
     Button postCategoryButton;
     EditText categoryNameInput;
+    EditText topicNameInput;
     ListView mainListView;
     JSONAdapter mJSONAdapter;
     String categoryName;
     Spinner spinner;
+    Spinner categoriesSpinnerInput;
     ArrayList<String> categoryArray = new ArrayList<String>();
     ArrayAdapter adapter;
+    final Context context = this;
+    int categoryId;
+    String categoryInput;
+    EditText topicInput;
 
     private static final String QUERY_URL = "http://conversationstarter.elasticbeanstalk.com/topics/?format=json";
-    private static final String CATEGORY_POST_URL = "http://conversationstarter.elasticbeanstalk.com/categories/";
+    private static final String TOPIC_POST_URL = "http://conversationstarter.elasticbeanstalk.com/topics/";
+    private static final String CATEGORY_URL = "http://conversationstarter.elasticbeanstalk.com/categories/";
     private static final String CATEGORIES_URL = "http://conversationstarter.elasticbeanstalk.com/categories/?format=json";
     private static final String QUERY_BY_CATEGORY_URL = "http://conversationstarter.elasticbeanstalk.com/topics/category/";
 
@@ -62,8 +72,56 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                // get prompts.xml view
+                LayoutInflater li = LayoutInflater.from(context);
+                View topicInputView = li.inflate(R.layout.topic_add, null);
+
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                        context);
+                alertDialogBuilder.setView(topicInputView);
+
+                topicInput = (EditText) topicInputView
+                        .findViewById(R.id.topic_input);
+
+                // Spinner for Add Topic Alert
+                categoriesSpinnerInput = (Spinner) topicInputView.findViewById(R.id.categories_spinner_input);
+                categoriesSpinnerInput.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                        categoryInput = parent.getItemAtPosition(position).toString();
+                        categoryId = getCategoryIdByName(categoryInput);
+                    }
+                    public void onNothingSelected(AdapterView<?> parent) {
+                        Log.d("hello", "hello");
+                    }
+                });
+                // Apply the adapter to the spinner
+                categoriesSpinnerInput.setAdapter(adapter);
+
+                // set dialog message
+                alertDialogBuilder
+                        .setCancelable(false)
+                        .setPositiveButton("OK",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,int id) {
+                                        // get user input and set it to result
+                                        // edit text
+                                        postTopic(topicInput.getText().toString(), categoryId);
+                                    }
+                                })
+                        .setNegativeButton("Cancel",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+
+                // create alert dialog
+                AlertDialog alertDialog = alertDialogBuilder.create();
+
+                // show it
+                alertDialog.show();
             }
         });
 
@@ -107,48 +165,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         spinner.setAdapter(adapter);
     }
 
-    private void postCategory(String categoryName) {
-        // Create a client to perform networking
-        AsyncHttpClient client = new AsyncHttpClient();
-        Context context = this.getApplicationContext();
 
-        // Have the client get a JSONArray of data
-        // and define how to respond
-        JSONObject jsonParams = new JSONObject();
-        try {
-            jsonParams.put("category_name", categoryName);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        StringEntity entity = null;
-        try {
-            entity = new StringEntity(jsonParams.toString());
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-        client.post(context, CATEGORY_POST_URL, entity, "application/json",
-                new JsonHttpResponseHandler() {
-
-                    @Override
-                    public void onSuccess(JSONArray jsonArray) {
-                        Log.e("yooo", CATEGORY_POST_URL);
-                        // Display a "Toast" message
-                        // to announce your success
-                        Toast.makeText(getApplicationContext(), "Success!", Toast.LENGTH_LONG).show();
-                    }
-
-                    @Override
-                    public void onFailure(int statusCode, Throwable throwable, JSONObject error) {
-                        // Display a "Toast" message
-                        // to announce the failure
-                        Toast.makeText(getApplicationContext(), "Error: " + statusCode + " " + throwable.getMessage(), Toast.LENGTH_LONG).show();
-
-                        // Log error message
-                        // to help solve any problems
-                        Log.e("Uhoh, it not work", statusCode + " " + throwable.getMessage());
-                    }
-                });
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -208,7 +225,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                         // Log error message
                         // to help solve any problems
-                        Log.e("Uhoh, it not work", statusCode + " " + throwable.getMessage());
+                        Log.e("query topics not work", statusCode + " " + throwable.getMessage());
                     }
                 });
     }
@@ -240,7 +257,98 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                         // Log error message
                         // to help solve any problems
-                        Log.e("Uhoh, it not work", statusCode + " " + throwable.getMessage());
+                        Log.e("query topic not work", statusCode + " " + throwable.getMessage());
+                    }
+                });
+    }
+
+    private void postCategory(String categoryName) {
+        // Create a client to perform networking
+        AsyncHttpClient client = new AsyncHttpClient();
+        Context context = this.getApplicationContext();
+
+        // Have the client get a JSONArray of data
+        // and define how to respond
+        JSONObject jsonParams = new JSONObject();
+        try {
+            jsonParams.put("category_name", categoryName);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        StringEntity entity = null;
+        try {
+            entity = new StringEntity(jsonParams.toString());
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        client.post(context, CATEGORY_URL, entity, "application/json",
+                new JsonHttpResponseHandler() {
+
+                    @Override
+                    public void onSuccess(JSONArray jsonArray) {
+                        Log.e("yooo", CATEGORY_URL);
+                        // Display a "Toast" message
+                        // to announce your success
+                        Toast.makeText(getApplicationContext(), "Success!", Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Throwable throwable, JSONObject error) {
+                        // Display a "Toast" message
+                        // to announce the failure
+                        Toast.makeText(getApplicationContext(), "Error: " + statusCode + " " + throwable.getMessage(), Toast.LENGTH_LONG).show();
+
+                        // Log error message
+                        // to help solve any problems
+                        Log.e("post category not work", statusCode + " " + throwable.getMessage());
+                    }
+                });
+    }
+
+    private void postTopic(String topicName, int categoryID) {
+        // Create a client to perform networking
+        AsyncHttpClient client = new AsyncHttpClient();
+        Context context = this.getApplicationContext();
+
+        // Have the client get a JSONArray of data
+        // and define how to respond
+        JSONObject jsonParams = new JSONObject();
+        try {
+            jsonParams.put("topic_name", topicName);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        try {
+            jsonParams.put("category_id", categoryID);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        StringEntity entity = null;
+        try {
+            entity = new StringEntity(jsonParams.toString());
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        client.post(context, TOPIC_POST_URL, entity, "application/json",
+                new JsonHttpResponseHandler() {
+
+                    @Override
+                    public void onSuccess(JSONArray jsonArray) {
+                        Log.e("yooo", TOPIC_POST_URL);
+                        // Display a "Toast" message
+                        // to announce your success
+                        Toast.makeText(getApplicationContext(), "Success!", Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Throwable throwable, JSONObject error) {
+                        // Display a "Toast" message
+                        // to announce the failure
+                        Toast.makeText(getApplicationContext(), "Error: " + statusCode + " " + throwable.getMessage(), Toast.LENGTH_LONG).show();
+
+                        // Log error message
+                        // to help solve any problems
+                        Log.e("post topic not work", statusCode + " " + throwable.getMessage());
                     }
                 });
     }
@@ -264,7 +372,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //                        categoryAdapter.updateData(jsonArray);
                         // add categories to an array list for Spinner
                         if (jsonArray != null) {
-                            for (int i=0;i<jsonArray.length();i++){
+                            for (int i = 0; i < jsonArray.length(); i++) {
                                 try {
                                     categoryArray.add(jsonArray.getJSONObject(i).getString("category_name"));
                                 } catch (JSONException e) {
@@ -283,10 +391,53 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                         // Log error message
                         // to help solve any problems
-                        Log.e("Uhoh, it not work", statusCode + " " + throwable.getMessage());
+                        Log.e("pop categories not work", statusCode + " " + throwable.getMessage());
                     }
                 });
 
+    }
+
+    private int getCategoryIdByName(String categoryName) {
+        // Create a client to perform networking
+        AsyncHttpClient client = new AsyncHttpClient();
+
+        // Have the client get a JSONArray of data
+        // and define how to respond
+        client.get(CATEGORY_URL + categoryName + "/?format=json",
+                new JsonHttpResponseHandler() {
+
+                    @Override
+                    public void onSuccess(JSONArray jsonArray) {
+                        // Display a "Toast" message
+                        // to announce your success
+                        Toast.makeText(getApplicationContext(), "Success!", Toast.LENGTH_LONG).show();
+
+                        // update the data in your custom method.
+//                        categoryAdapter.updateData(jsonArray);
+                        // add categories to an array list for Spinner
+                        if (jsonArray != null) {
+                            for (int i=0;i<jsonArray.length();i++){
+                                try {
+                                    categoryId = jsonArray.getJSONObject(i).getInt("id");
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Throwable throwable, JSONObject error) {
+                        // Display a "Toast" message
+                        // to announce the failure
+                        Toast.makeText(getApplicationContext(), "Error: " + statusCode + " " + throwable.getMessage(), Toast.LENGTH_LONG).show();
+
+                        // Log error message
+                        // to help solve any problems
+                        Log.e("category id not work", statusCode + " " + throwable.getMessage());
+                    }
+                });
+        return categoryId;
     }
 
     @Override
